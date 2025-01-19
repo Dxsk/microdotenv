68e120c5d55d56f2e4eca238469d837a243b540d
Daihyxsk <43427519+Dxsk@users.noreply.github.com>
2025-01-19 13:14:49 +0100

    add install.bash

---
+++ install.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+curl https://raw.githubusercontent.com/Dxsk/microdotenv/refs/heads/main/.vimrc --silent > .vimrc
+curl https://raw.githubusercontent.com/Dxsk/microdotenv/refs/heads/main/.custom_bash_rc --silent > .custom_bash_rc
+echo "source .custom_bash_rc" >> .bashrc
+bash
